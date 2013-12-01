5db531d76b9d5dde82973a53c0fc738af37e94f9
tomo taka <tomo_taka01@hotmail.com>
2013-12-01 12:44:39 +0900

    delete unused methd

--- src/filetreeviewsample/FileCopyTask.java
+++ src/filetreeviewsample/FileCopyTask.java
@@ -1,7 +1,6 @@
 package filetreeviewsample;
 
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import javafx.concurrent.Task;
@@ -14,7 +13,6 @@ import javafx.concurrent.Task;
 public class FileCopyTask extends Task<Void> {
     private Path source;
     private Path target;
-    private boolean isReplaceCopy;
 
     public FileCopyTask(Path source, Path target) {
         this.source = source;
@@ -23,13 +21,7 @@ public class FileCopyTask extends Task<Void> {
     
     @Override
     protected Void call() throws Exception {
-        this.isReplaceCopy = Files.exists(this.target, LinkOption.NOFOLLOW_LINKS);
         Files.copy(this.source, this.target, StandardCopyOption.REPLACE_EXISTING);
         return null;
     }
-
-    public boolean isReplaceCopy() {
-        return isReplaceCopy;
-    }
-    
 }
